3fc72dcf597454e969bf904afdd0a264f34f9b92
lonelil <51315646+lonelil@users.noreply.github.com>
2024-05-06 01:28:26 +0800

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,16 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+    .setup(|app| {
+        let window = app.get_window("main").unwrap();
+        let _ = window.eval("window.location.replace('https://youtube.com/tv')");
+
+        Ok(())
+      })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
